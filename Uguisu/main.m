4f4b4c27a0c2726ba484067f88d7403a3aeff748
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2015-03-05 22:35:16 +0900

    Initial Commit

---
+++ Uguisu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Uguisu
+//
+//  Created by Kenzo on 2015/03/05.
+//  Copyright (c) 2015年 Uguisu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
